ccd5a67306ea9050ece9e744e74abb976b93df61
OualidZM <ozaaj@cifpfbmoll.eu>
2021-11-20 15:50:55 +0100

    docs: :construction: JS
    Added some JS code to show the layout form

--- src/js/addEvent.js
+++ src/js/addEvent.js
@@ -1,3 +1,16 @@
-document.getElementById('addEventStyle').addEventListener('click',()=>{
-    alert('ee');
-})
+    const layout = document.querySelector(".addEventLayout");
+    document.getElementById("addEventStyle").addEventListener('click',layoutToggle);
+    document.getElementById("closeLayout").addEventListener('click',layoutToggle);
+    window.addEventListener("click", windowOnClick);
+
+    function layoutToggle() {
+        layout.classList.toggle("show-modal");
+    }
+
+    function windowOnClick(event) {
+        if (event.target === layout) {
+            layoutToggle();
+        }
+    }
+
+	
