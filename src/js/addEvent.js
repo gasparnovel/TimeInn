57491911d1ba68543dce0aec612220ce3910ad40
OualidZM <ozaaj@cifpfbmoll.eu>
2021-11-20 19:56:47 +0100

    docs: :construction: JS
    Adding a counter to check if the layout to add events appears when the button is clicked, and adding an index file

--- src/js/addEvent.js
+++ src/js/addEvent.js
@@ -1,12 +1,19 @@
-    const layout = document.querySelector(".addEventLayout");
+    var contador = 0;
+    const container = document.querySelector('body');
+    const layout = document.getElementById("backEvent");
     document.getElementById("addEventStyle").addEventListener('click',layoutToggle);
     document.getElementById("closeLayout").addEventListener('click',layoutToggle);
     window.addEventListener("click", windowOnClick);
-
     function layoutToggle() {
-        layout.classList.toggle("show-modal");
+        if(contador == 0){
+            document.getElementById("backEvent").style.visibility = "visible"
+            contador++;
+        }else{
+            document.getElementById("backEvent").style.visibility = "hidden";
+            contador = 0;
+        }
     }
-
+    
     function windowOnClick(event) {
         if (event.target === layout) {
             layoutToggle();
