6efa76a10abccfa18a07f71e15afb90453fb70ae
OualidZM <ozaaj@cifpfbmoll.eu>
2021-11-23 20:58:05 +0100

    docs: :construction: JS
    Added objects into the array, and checking if the button of the form was clicked, if was clicked, a new object of the values of the form will be added to the array

--- src/js/addEvent.js
+++ src/js/addEvent.js
@@ -1,10 +1,24 @@
+ var  tt = [];
+ var sorted = [];
+ 
+ 
+ document.addEventListener('DOMContentLoaded',()=>{
+    tt = [{nameEvent:'Hiking',dateEvent:'2020-12-15',descriptionEvent:'hiking descr',imgEvent:'img Hiking'},
+    {nameEvent:'wedding',dateEvent:'2020-12-30',descriptionEvent:'wedding descr',imgEvent:'wedding Hiking'},
+    {nameEvent:'pary',dateEvent:'2020-12-10',descriptionEvent:'pary descr',imgEvent:'pary Hiking'}]
+
+    sorted = tt.sort((a,b) =>{
+    return (a.dateEvent > b.dateEvent) ? 1 : -1
+    })
+    console.log(sorted);
+ })   
+    
     var contador = 0;
-    const container = document.querySelector('body');
     const layout = document.getElementById("backEvent");
     document.getElementById("addEventStyle").addEventListener('click',layoutToggle);
     document.getElementById("closeLayout").addEventListener('click',layoutToggle);
     window.addEventListener("click", windowOnClick);
-    function layoutToggle() {
+ function layoutToggle() {
         if(contador == 0){
             document.getElementById("backEvent").style.visibility = "visible"
             contador++;
@@ -14,10 +28,21 @@
         }
     }
     
-    function windowOnClick(event) {
+ function windowOnClick(event) {
         if (event.target === layout) {
             layoutToggle();
         }
     }
 
-	
+    const btnsubm = document.getElementById("btnSubmit").addEventListener('click',(e)=>{
+        e.preventDefault();
+        let arrFormValues = {
+            nameEvent: document.getElementById("eventName").value,
+            dateEvent: document.getElementById("dateValue").value,
+            descriptionEvent: document.getElementById("eventDescription").value,
+            imgEvent: "party eating img"
+        }
+        sorted.unshift(arrFormValues);
+        layoutToggle();
+        console.log(sorted);
+    })
---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+import {layoutToggle,windowOnClick } from './addEvent.js';
+
+
