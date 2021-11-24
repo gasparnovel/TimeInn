a66968530b463f555d68504e86a5f92bb2d1c8b1
gasparnovel <gnovel@cifpfbmoll.eu>
2021-11-24 21:05:46 +0100

    feat: :sparkles: Header and Footer
    Added header and footer functionality in separate files

---
+++ src/js/load_header_footer.js
@@ -0,0 +1,18 @@
+document.getElementById("myHeader").innerHTML =
+    "<nav>" +
+    "<a href='../index.html'><h1 class='logo'>Evacten</h1></a>" +
+    "<ol class='nav-box'>" +
+    "<a href='./eventos.html'><li class='navLinkStyle'>Eventos</li></a>" +
+    "<a href='./formulario.html'><li class='navLinkStyle'>Formulario</li></a>" +
+    "<a href='./aboutUs.html'><li class='navLinkStyle'>About Us</li></a>" +
+    "<a href='./signIN.html'><li class='navLinkStyle'>Sign In</li></a>" +
+    "</ol>" +
+    "</nav>";
+document.getElementById("myFooter").innerHTML =
+    "<footer class='footerStyle'>" +
+    "<li class='fotLinkStyle'>c 2021 Evacten Inc.</li>" +
+    "<li class='fotLinkStyle'>Palma de Mallorca</li>" +
+    "<li class=''>Icono1</li>" +
+    "<li class=''>Icono2</li>" +
+    "<li class=''>Icono3</li>" +
+    "</footer>";
