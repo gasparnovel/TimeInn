bb5b39e7365dd94da21f53a5e1e410f64a19cc2c
OualidZM <ozaaj@cifpfbmoll.eu>
2021-12-12 20:30:30 +0100

    docs: :construction: JS
    now emailChecker can check if an email exists or not

--- src/js/emailCheker.js
+++ src/js/emailCheker.js
@@ -1,4 +1,4 @@
-let registeredUsers = [
+export let registeredUsers = [
     {name:"Jose",Email:"Jose53@gmail.com",password:"123456789Josee"},
     {name:"Maria",Email:"Lapilli01@gmail.com",password:"45qwkdaq."},
     {name:"Pablo",Email:"pablo_el_mota@gmail.com",password:"pabloelcani13"}
@@ -19,16 +19,20 @@ export function emailCheker(){
 
     }else if(regexResultEmail == false){
         emPosition.style.border = "2px red solid";
-                showError.innerHTML = "Add a correct email, and try again"
+        showError.innerHTML = "Add a correct email, and try again"
         return false
 
-    }else{
+    }
+    else{
         let target = registeredUsers.find(e=>e.Email == emValue)
         if(target){
             emPosition.style.border = "2px red solid";
+            showError.innerHTML = "Email already in use";
         }
         else{
             emPosition.style.border = "2px green solid";
+            showError.innerHTML = "";
+
             
         }
         // emPosition.style.border = "2px green solid";
