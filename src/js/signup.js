ffbda97e270199801102c7ea372771d968b2979d
OualidZM <ozaaj@cifpfbmoll.eu>
2021-12-12 20:31:16 +0100

    docs: :construction: JS
    Corrected  some bugs in the main file

--- src/js/signup.js
+++ src/js/signup.js
@@ -1,25 +1,63 @@
-import { emailCheker } from "./emailCheker.js";
+import { emailCheker,registeredUsers } from "./emailCheker.js";
 import { passwordCheker } from "./passwordCheker.js";
 import { submitCheck } from "./submitCheck.js"
 import { nameChecker } from "./nameChecker.js";
 
-document.activeElement.addEventListener('click',checkData)
 document.getElementById('login').addEventListener('click',()=>{
+    
     submitCheck(nameChecker(),emailCheker(),passwordCheker())
+    if(nameChecker() == true && emailCheker() == true && passwordCheker() == true){
+        let addedSucc = document.getElementById("addedSucc")
+
+        let emPosition = document.getElementById('emailInput')
+
+        let emValue = document.getElementById('emailInput').value
+        let newUser = {name:"test",Email:emValue,password:"testt"};
+
+        let target = registeredUsers.find(e=>e.Email == emValue)
+
+        if(target){
+            showError.innerHTML = "Email already in use";
+
+            emPosition.style.border = "2px red solid";
+            console.log("Email already in use")
+        }
+        else{
+            registeredUsers.push(newUser);
+            addedSucc.innerHTML = "Account Created"
+        }
+    }else{
+        console.log("not true")
+        addedSucc.innerHTML = ""
+
+    }
+    console.log(registeredUsers);
+
 })
 
-function checkData(e){
+
+
+document.activeElement.addEventListener('click',(e)=>{
+
     let emPosition = document.getElementById('emailInput')
     let passPosition = document.getElementById('passwordInput')
     let namePosition = document.getElementById('nameInput');
-
+    
     if (document.readyState == "complete") {
-        if(e.target != emPosition || e.target != passPosition || e.target != namePosition)  { 
-            nameChecker()
-            emailCheker()
-            passwordCheker()
-        }else{
+        if(e.target == emPosition || e.target == passPosition || e.target == namePosition)  { 
+            // console.log(e.target)
+            // console.log("no checking")
+            
+
+        }else if(e.target == document.getElementById("login")){
+
+        }
+        else{
+            submitCheck(nameChecker(),emailCheker(),passwordCheker())
+            // console.log(e.target)
+            // console.log("checking")
+
         }
     }else{
     }
-}
+})
